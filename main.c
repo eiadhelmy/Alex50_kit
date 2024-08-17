0bf5499f1cd0ba921283ca4fd5460dfe8f3ccf43
eiadhelmy <eiadhelmy7@gmail.com>
2024-08-17 16:49:55 +0200

    the start point

---
+++ main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: IT Solutions
+ *
+ * Created on 17 ?????, 2024, 04:30 ?
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
